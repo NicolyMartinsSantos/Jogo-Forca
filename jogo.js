fe0fd54231ee1e3e0741a57c11133fcf0f207205
Nicoly Martins Santos <nimartinns@gmail.com>
2023-11-13 12:20:49 -0300

    alterações

---
+++ jogo.js
@@ -0,0 +1,51 @@
+const elementos = {
+    telaInicial: document.getElementById('inicial'),
+    telaCadastro: document.getElementById('cadastro'),
+    telaJogo: document.getElementById('jogo'),
+    telaMensagem: document.querySelector('.mensagem'),
+    textoMensagem: document.querySelector('.mensagem .texto'),
+    teclado: document.querySelector('teclado'),
+    palavra: document.querySelector(' .palavra'),
+    dica: document.querySelector(' .dica'),
+botoes:{
+    facil: document.querySelector(' .botao-facil'),
+    medio: document.querySelector(' .botao-medio'),
+    dificil:document.querySelector(' .botao-dificil'),
+    cadastrar: document.querySelector(' .botao-cadastrara'),
+    realizarCadastro: document.querySelector(' .botao-realizar-cadastro'),
+    voltar: document.querySelector(' .botao-voltar'),
+    reiniciar: document.querySelector('.reiniciar'),
+},
+campos: {
+    dificuldade: {
+        facil: document.getElementById('facil'),
+        medio: document.getElementById('medio'),
+        dificil: document.getElementById('dificil'),
+
+    },
+    palavra: document.getElementById('palavra'),
+    dica: document.getElementById('dica')
+},
+boneco:[
+    document.querySelector('.boneco-cabeca'),
+    document.querySelector('.boneco-corpo'),
+    document.querySelector('.boneco-braco-esquerdo'),
+    document.querySelector('.boneco-braco-direito'),
+    document.querySelector('.boneco-perna-esquerda'),
+    document.querySelector('boneco-perna-direita'),
+],
+
+const elementos = {
+
+    telaInicial: document.getElementById('inicial'),
+    telaCadastro: document.getElementById('cadastro'),
+    telaJogo: document.getElementById('jogo'),
+    telaMensagem: document.querySelector('.mensagem'),
+    textoMensagem: document.querySelector('.mensagem .texto'),
+    teclado: document.querySelector('.teclado'),
+    palavra: document.querySelector('.palavra'),
+    dica: document.querySelector('.dica'),
+
+}
+
+};
